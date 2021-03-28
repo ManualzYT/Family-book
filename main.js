cfe862a50c86c96a7c008acecac3d6794c8eb0e2
ManualzYT <75396652+ManualzYT@users.noreply.github.com>
2021-03-28 10:16:41 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -8,4 +8,6 @@ var images = [
 var i = 0; 
 function nextslide() {
 if (i == 4) { i=0;}
-} 
+}
+var array_names_of_family = [ "Manvel Simonyan" , "Inessa Simonyan" , "Harutyun Simonyan" , "Narine Marandyan" ]; 
+var array_images_of_family = [ "maxresdefault.jpg" , "1580434668566 (1).jpg" , "download.jpg" , "10906575_732597830169922_4919127290685897003_n.jpg" ]; 
