5e16913a012a9021fb96afd9eafa4b2575f08b45
ManualzYT <75396652+ManualzYT@users.noreply.github.com>
2021-03-27 18:40:21 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
   
-function nextslide() 
-var array_for_images = document.getElementById("Manvel_Simonyan").value;
+var images = [
+    "maxresdefault.jpg",
+    "1580434668566 (1).jpg",
+    "download.jpg",
+    "10906575_732597830169922_4919127290685897003_n.jpg",
+    ];
+var i = 0; 
+function nextslide() {
+if (i == 4) { i=0;}
+} 
